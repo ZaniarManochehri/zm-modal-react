9a964c3f27264ada124c7e53d54f189a179a2864
Zaniar <zaniar.manochehri@gmail.com>
2022-08-28 10:15:33 +0430

    Improv bottom sheet modal animation

--- src/components/ModalContent.tsx
+++ src/components/ModalContent.tsx
@@ -2,7 +2,7 @@ import { Button } from "components";
 
 type Props = {
   handleClose: () => void;
-  message: string;
+  type: "Modal" | "Bottom Sheet" | "Full Screen Modal";
   closeButtonType:
     | "link"
     | "color"
@@ -13,10 +13,10 @@ type Props = {
     | undefined;
 };
 const ModalContent: React.FC<Props> = (props) => {
-  const { handleClose, message, closeButtonType } = props;
+  const { handleClose, type, closeButtonType } = props;
   return (
     <div className="modal-content">
-      <span>{`Hi ${String.fromCodePoint(0x1f44b)}, This is a ${message}!`}</span>
+      <span>{`Hi ${String.fromCodePoint(0x1f44b)}, This is a ${type}!`}</span>
       <Button width={120} onClick={handleClose} type={closeButtonType}>
         <span>Close</span>
       </Button>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,74 +1,64 @@
 import { useState } from "react";
 import { Button, Modal, ModalContent } from "components";
 
-type buttonType =
-  | "link"
-  | "color"
-  | "primary"
-  | "secondary"
-  | "outline"
-  | "danger"
-  | undefined;
 const Home: React.FC = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const [showBottomSheet, setShowBottomSheet] = useState<boolean>(false);
   const [showFullScreenModal, setShowFullScreenModal] =
     useState<boolean>(false);
-  const [modalMessage, setModalMessage] = useState<string>("");
-  const [closeModalButtonType, setCloseModalButtonType] = useState<buttonType>();
-
-  const handleShowModal = () => {
-    setShowModal(true);
-    setModalMessage("Modal");
-    setCloseModalButtonType("primary");
-  };
-
-  const handleShowBottomSheet = () => {
-    setShowBottomSheet(true);
-    setModalMessage("Bottom Sheet");
-    setCloseModalButtonType("color");
-  };
-
-  const handleShowFullScreenModal = () => {
-    setShowFullScreenModal(true);
-    setModalMessage("Full Screen Modal");
-    setCloseModalButtonType("secondary");
-  };
-
-  const handleCloseModal = () => {
-    if (showModal) {
-      setShowModal(false);
-    } else if (showBottomSheet) {
-      setShowBottomSheet(false);
-    } else {
-      setShowFullScreenModal(false);
-    }
-  };
 
   return (
     <div className="home-container">
-      <Button width={220} type="primary" onClick={handleShowModal}>
+      <Button width={220} type="primary" onClick={() => setShowModal(true)}>
         Modal
       </Button>
-      <Button width={220} onClick={handleShowBottomSheet}>
+      <Button width={220} onClick={() => setShowBottomSheet(true)}>
         Bottom Sheet
       </Button>
-      <Button width={220} type="secondary" onClick={handleShowFullScreenModal}>
+      <Button
+        width={220}
+        type="secondary"
+        onClick={() => setShowFullScreenModal(true)}
+      >
         Full Screen Modal
       </Button>
 
       <Modal
-        show={showModal || showBottomSheet || showFullScreenModal}
-        onClose={handleCloseModal}
+        show={showModal}
+        onClose={() => setShowModal(false)}
         defaultHeader
         title="Modal"
-        isBottomSheet={showBottomSheet}
-        fullScreen={showFullScreenModal}
       >
         <ModalContent
-          message={modalMessage}
-          handleClose={handleCloseModal}
-          closeButtonType={closeModalButtonType}
+          type="Modal"
+          handleClose={() => setShowModal(false)}
+          closeButtonType="primary"
+        />
+      </Modal>
+      <Modal
+        show={showBottomSheet}
+        onClose={() => setShowBottomSheet(false)}
+        defaultHeader
+        title="Modal"
+        isBottomSheet
+      >
+        <ModalContent
+          type="Bottom Sheet"
+          handleClose={() => setShowBottomSheet(false)}
+          closeButtonType="color"
+        />
+      </Modal>
+      <Modal
+        show={showFullScreenModal}
+        onClose={() => setShowFullScreenModal(false)}
+        defaultHeader
+        title="Modal"
+        fullScreen
+      >
+        <ModalContent
+          type="Full Screen Modal"
+          handleClose={() => setShowFullScreenModal(false)}
+          closeButtonType="secondary"
         />
       </Modal>
     </div>
